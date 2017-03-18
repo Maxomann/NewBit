4aaac74682c5e297fda98e8e5597a0468c407182
Kay Gonschior <kay.gonschior@googlemail.com>
2017-03-18 21:40:40 +0100

    remove THOR

--- CoreEngines/src/stdafx.h
+++ CoreEngines/src/stdafx.h
@@ -3,8 +3,6 @@
 #include <NewBitPackageManager\src\PackageManager.h>
 #include <NewBitTexturePacker\src\NewBitTexturePacker.h>
 
-#include <Thor\Input.hpp>
-
 #include <TGUI\TGUI.hpp>
 
 #define DLL_EXPORT __declspec(dllexport)
