56c3d1cbb0c70d65c07852c6f0796cb3110b1e8e
Kay Gonschior <kay.gonschior@googlemail.com>
2017-05-07 21:01:52 +0200

    much change

---
+++ src/NewBitEntity/stdafx.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <typeinfo>
+#include <typeindex>
+#include <list>
+#include <memory>
+#include <map>
+#include <unordered_map>
+#include <vector>
+#include <algorithm>
+#include <functional>
+#include <stdexcept>
+#include <utility>
+
+#include <NewBitSignals\Signal.h>
+
+#define DLL_EXPORT __declspec(dllexport)
